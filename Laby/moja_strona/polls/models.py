4974b41f6ccebd14f308fe093f2391af9761b7a2
Autentyk <162135@student.uwm.edu.pl>
2024-10-17 11:36:35 +0200

    lab3

--- Laby/moja_strona/polls/models.py
+++ Laby/moja_strona/polls/models.py
@@ -27,3 +27,30 @@ class Person(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Stanowisko(models.Model):
+    nazwa = models.CharField(max_length=100, blank=False, null=False)
+    opis = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nazwa
+
+
+class Osoba(models.Model):
+    class Plec(models.IntegerChoices):
+        KOBIETA = 1, 'Kobieta'
+        MEZCZYZNA = 2, 'Mężczyzna'
+        INNE = 3, 'Inne'
+
+    imie = models.CharField(max_length=100, blank=False, null=False)
+    nazwisko = models.CharField(max_length=100, blank=False, null=False)
+    plec = models.IntegerField(choices=Plec.choices, blank=False, null=False)
+    stanowisko = models.ForeignKey(Stanowisko, on_delete=models.CASCADE)
+    data_dodania = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.imie} {self.nazwisko}"
+
+    class Meta:
+        ordering = ['nazwisko']
